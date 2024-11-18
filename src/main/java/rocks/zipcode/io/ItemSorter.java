1f9de82a37460fd3e797da252a0f15a8625c0d9b
Alyssa Resh <aresh@zipcoders-mbp.mynetworksettings.com>
2024-11-17 23:48:50 -0500

    worked on itemsorter class, just need to write the extra tests

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,23 @@
 package rocks.zipcode.io;
 
+import rocks.zipcode.io.comparators.NameComparator;
+
+import java.util.Arrays;
 import java.util.Comparator;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+    private Item[] itemsToSort;
+    Comparator<Item> comparator;
+
     public ItemSorter(Item[] items) {
+        this.itemsToSort = items;
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        Arrays.sort(itemsToSort, comparator);
+        return itemsToSort;
     }
 }
